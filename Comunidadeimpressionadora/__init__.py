b87663c95a8babcda45bcb5fc9523faf75c69712
RomeuRiffatti <149801612+RomeuRiffatti@users.noreply.github.com>
2024-07-17 13:34:12 -0300

    Update __init__.py
    adicionando código usando o sqlalchemy para criar um banco de dados caso não exista

--- Comunidadeimpressionadora/__init__.py
+++ Comunidadeimpressionadora/__init__.py
@@ -1,4 +1,5 @@
 
+
 ## Esse arquvi init basicamente importa o app e coloca ele pra rodar
 ## aqui tem que:
 #   1. importar o app
@@ -11,7 +12,7 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
 from flask_login import LoginManager
 import os
-
+import sqlalchemy
 
 app = Flask(__name__)
 
@@ -29,6 +30,18 @@ bcrypt = Bcrypt(app)
 login_manager = LoginManager(app)
 login_manager.login_view = 'login'  ## para uso do login_required. ele diz para que função vai redirecionar quando o usuario tentar acessar uma pagina tem login¨_required
 login_manager.login_message_category = 'alert-info' ## classe do bootstrap de caixa azul
+
+from Comunidadeimpressionadora import models
+engine = sqlalchemy.create_engine(app.config['SQLALCHEMY_DATABASE_URI'])
+inspector = sqlalchemy.inspect(engine)
+if not inspector.has_table("usuario"):
+    with app.app_context():
+        database.drop_all()
+        database.create_all()
+        print("base de dados criada")
+else:
+    print("base de dados ja existente")
+
 
 ##Esse import está aqui em baixo porque primeiro o init acessa a pasta e inicia o programa e depois ele pega os routes com a pasta já aberta, se importar antes a pasta de routes ainda nao foi aberta. 
 ## o app ainda nao foi criado
